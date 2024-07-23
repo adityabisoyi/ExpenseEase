a9640109fc80d28aebee8deb2fc85e10d0071006
adityabisoyi <adibisoyi01@gmail.com>
2024-07-23 15:19:40 +0530

    Express server setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,4 @@
+import app from "./app.js";
 import dbConnect from "./db/dbConnection.js";
 import dotenv from 'dotenv'
 
@@ -6,3 +7,13 @@ dotenv.config({
 })
 
 dbConnect()
+.then(() => {
+  const portNo = process.env.PORT || 8000
+  app.listen(portNo, () => {
+    console.log(`Server is running on : ${portNo}`)
+  })
+})
+.catch((err) => {
+  console.log("MongoDB connect error : ", err)
+  process.exit(1)
+})
