34277c347923003934d9c835ef529734e6d15a05
Aditya Bisoyi <adibisoyi01@gmail.com>
2024-07-23 11:50:30 +0530

    Setup and db connect

---
+++ server/src/db/dbConnection.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+import { DB_NAME } from '../constants.js'
+
+const dbConnect = async () => {
+  try {
+    const dbConnection = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+    console.log("MongoDB connected !!! Host : ", dbConnection.connection.host)
+  } catch (error) {
+    console.log("MongoDB connection error : ", error)
+    process.exit(1)
+  }
+}
+
+export default dbConnect
